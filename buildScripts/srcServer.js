49c45bad95247dd22c4b48f2af9d840e02531307
Alexander Trapp <atrapp@Alexanders-MBP.fritz.box>
2016-11-22 14:49:09 +0100

    Finished Chapter 9

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -23,6 +23,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function (req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id":1, "firstName": "Bob", "lastName": "Builder", "email": "bob@gmail.com"},
+    {"id":2, "firstName": "Bibi", "lastName": "Blocksberg", "email": "hexhex@yahoo.com"},
+    {"id":3, "firstName": "Wendy", "lastName": "Wallace", "email": "wendy@gmail.com"}
+  ]);
+});
+
 app.listen(port, function (err) {
   if (err) {
     console.log(err);
